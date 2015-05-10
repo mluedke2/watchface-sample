506667239aef3b5e6b714e4eb31284baef94fd14
mluedke2 <mluedke2@gmail.com>
2015-05-09 21:03:02 -0700

    set watch face style

--- wear/src/main/java/com/updowninteractive/watchface/WatchFaceService.java
+++ wear/src/main/java/com/updowninteractive/watchface/WatchFaceService.java
@@ -1,6 +1,8 @@
 package com.updowninteractive.watchface;
 
 import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.support.wearable.watchface.WatchFaceStyle;
+import android.view.SurfaceHolder;
 
 public class WatchFaceService extends CanvasWatchFaceService {
 
@@ -10,6 +12,14 @@ public class WatchFaceService extends CanvasWatchFaceService {
   }
 
   private class Engine extends CanvasWatchFaceService.Engine {
-
+    @Override
+    public void onCreate(SurfaceHolder holder) {
+      setWatchFaceStyle(new WatchFaceStyle.Builder(WatchFaceService.this)
+          .setCardPeekMode(WatchFaceStyle.PEEK_MODE_SHORT)
+          .setAmbientPeekMode(WatchFaceStyle.AMBIENT_PEEK_MODE_HIDDEN)
+          .setBackgroundVisibility(WatchFaceStyle.BACKGROUND_VISIBILITY_INTERRUPTIVE)
+          .setShowSystemUiTime(false)
+          .build());
+    }
   }
 }
