d445883424b937e69efcfa28926fa5b71ed70f2f
mluedke2 <mluedke2@gmail.com>
2015-05-11 01:29:30 -0700

    get date to display

--- wear/src/main/java/com/updowninteractive/watchface/WatchFace.java
+++ wear/src/main/java/com/updowninteractive/watchface/WatchFace.java
@@ -10,11 +10,14 @@ import java.util.GregorianCalendar;
 
 public class WatchFace {
   private static final String TIME_FORMAT = "%02d:%02d";
+  private static final String DATE_FORMAT = "%02d.%02d.%d";
+  private final Paint datePaint;
   private final Paint timePaint;
   private final int backgroundColor;
 
-  WatchFace(Paint timePaint, int backgroundColor) {
+  WatchFace(Paint timePaint, Paint datePaint, int backgroundColor) {
     this.timePaint = timePaint;
+    this.datePaint = datePaint;
     this.backgroundColor = backgroundColor;
   }
 
@@ -24,7 +27,13 @@ public class WatchFace {
     timePaint.setColor(resources.getColor(R.color.watch_face_time));
     timePaint.setTextSize(resources.getDimension(R.dimen.time_size));
     timePaint.setAntiAlias(true);
-    return new WatchFace(timePaint, resources.getColor(R.color.watch_face_fill));
+
+    Paint datePaint = new Paint();
+    datePaint.setColor(resources.getColor(R.color.watch_face_date));
+    datePaint.setTextSize(context.getResources().getDimension(R.dimen.date_size));
+    datePaint.setAntiAlias(true);
+
+    return new WatchFace(timePaint, datePaint, resources.getColor(R.color.watch_face_fill));
   }
 
   public void draw(Canvas canvas, Rect bounds) {
@@ -41,5 +50,10 @@ public class WatchFace {
 
     canvas.drawColor(backgroundColor);
     canvas.drawText(timeText, timeCenterX, timeCenterY, timePaint);
+
+    String dateText = String.format(DATE_FORMAT, time.get(Calendar.DAY_OF_MONTH), time.get(Calendar.MONTH) + 1, time.get(Calendar.YEAR));
+    float dateCenterX = centerX - (datePaint.measureText(dateText) / 2.0f);
+    float dateCenterY = timeCenterY - (boundingBox.height() + 10.0f);
+    canvas.drawText(dateText, dateCenterX, dateCenterY, datePaint);
   }
 }
